4798bc26ceed391b72d0d534dce0b6e67bfa1d4b
ana <ana@localhost>
2020-11-01 00:52:47 +0100

    devoirs

--- ProgrammationObjetGroupware_28503310/src/atelier05/Promotion.java
+++ ProgrammationObjetGroupware_28503310/src/atelier05/Promotion.java
@@ -1,9 +1,10 @@
 package atelier05;
 
 public class Promotion {
+	String nom, prenom;
+	int numeroEtudiant, totaldesetudiants,moyenne_etudiant, moyennegenerale;
 	
-
-	public Promotion() {
+	public Promotion(String nom, String prenom, int numeroEtudiant, int totaldesetudiants, int moyenne_etudiant, int moyennegenerale) {
 		// TODO Auto-generated constructor stub
 	}
 
---
+++ ProgrammationObjetGroupware_28503310/src/devoirs/devoir2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package devoirs;
+
+/**
+ * @author ana
+ *
+ */
+public class devoir2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
