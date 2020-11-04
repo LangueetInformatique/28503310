074bd8b74d8abcb0a23ad7e4e541926909986a25
ana <ana@localhost>
2020-11-04 19:59:41 +0100

    Travaux Pratiques n° 4. Structures de contrôle. Exercice 4

--- ProgrammationObjetGroupware_28503310/src/devoirs/devoir1.java
+++ ProgrammationObjetGroupware_28503310/src/devoirs/devoir1.java
@@ -2,11 +2,15 @@ package devoirs;
 
 import java.util.Arrays;
 
+/** Exercice 4 : Ajouter les instructions structurées par des branchements 
+ * conditionnels permettant d’obtenir la médiane des variables 
+ * p1, p2, p3, p4, p5 dans la variable p6.*/
+
 public class devoir1 {
 
 	public static void main(String[] args) {
 		int p1 = 10, p2 = -1, p3 = 9, p4 = 4, p5 = 8;
-
+		
 		mediane(p1, p2, p3, p4, p5);
 		mediane(p2, p3, p4, p5, p1);
 		mediane(p3, p4, p5, p1, p2);
@@ -17,21 +21,19 @@ public class devoir1 {
 
 	public static void mediane(int p1, int p2, int p3, int p4, int p5) {
 		
-		int p6 = 0;
+		int p6;
 		int arreglo[] = {p1, p2, p3, p4, p5}; // on va organiser les chiffres
 		
 		Arrays.sort(arreglo);
 
-        int mediane;
         int moitie = arreglo.length / 2; // pour trouver la moitié de la serie
         
         if (arreglo.length % 2 == 0) { // si le tableau est pair, il faut faire la moyenne des deux chiffres au centre
-            mediane = (arreglo[moitie - 1] + arreglo[moitie]) / 2;
+        	p6 = (arreglo[moitie - 1] + arreglo[moitie]) / 2;
         } else {
-            mediane = arreglo[moitie];
+        	p6 = arreglo[moitie];
         }
-        p6 = mediane;
-
+        
         // On peut imprimer la serie ordonée
         /**System.out.println("El arreglo es: ");
         for (int x = 0; x < arreglo.length; x++) {
