37ad45e509f4441f5e8f9dfa477bde83da5a034b
anita <anita@DESKTOP-LQ2OGKV>
2020-11-19 11:01:25 +0100

    Atelier 08

---
+++ ProgrammationObjetGroupware_28503310/src/atelier08/PileFace.java
@@ -0,0 +1,18 @@
+package atelier08;
+
+public class PileFace {
+	
+	public static void main(String[]args) {
+		Aleat a = new Aleat(2);
+		int pile = 0;
+		int face = 0;
+		for(int i = 0; i<10; i++) {
+			if (a.get()==1)
+				pile ++;
+			else
+				face++;
+		}
+		System.out.println("Pile : "+pile + " | " +"Face : "+face);
+	}
+
+}
