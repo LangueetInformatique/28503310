ae99b779b4db485093ef4b6cb0d336380f042b35
ana <ana@DESKTOP-LQ2OGKV>
2021-02-10 18:41:48 +0100

    update

---
+++ ProgrammationGénériqueEtConceptionObjet/TD1/MonPremierProgramme.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : tp1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std; // para imprimir 
+
+int main() {
+	cout << "Ana" << endl; // prints votre nom
+	return 0;
+}
